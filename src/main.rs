a026cb964b851ad24fbb2be7b69cdebf9e23b7a1
trionprog <trionprog@gmail.com>
2017-01-10 22:19:43 +0300

    timer's architecture

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,14 @@
 
-#![deny(unsafe_code)]
+//#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 #![feature(const_fn)]
+#![feature(asm)]
 
 #[macro_use]
 extern crate f3;
 
-//use core::mem;
-
-/*
-
-struct E0;
-
-struct Spi{
-    rate:i8,
-}
-
-static spi:Spi=Spi::unconfigured();
-
-use f3::stm32::pin::Pin;
-
-fn config<C:f3::stm32::pin::Pin>(c:C){
-    panic!("Hello, world! {}",f3::stm32::pin::Pin::get_index(&c));
-}
-
-*/
-
-
+use f3::Box;
 
 use f3::gpioe::{E8,E9};
 use f3::output::{Output,OutputFun};
@@ -36,12 +17,19 @@ use f3::output::{Output,OutputFun};
 //use f3::delay;
 
 static led1:Output<E8>=Output::new();
-//static led2:Output<E9>=Output::new();
+//static led2:Output<E9>=Output::new(); // E9 do not support Output function!
 
+use f3::stm::timer::{BasicTimer, BasicTimerConfig};
+use f3::timer6::Timer6;
 
+static tim6:BasicTimer<Timer6>=BasicTimer::new(
+    BasicTimerConfig{
+        prescaler:8000,
+    }
+);
 
 /*
-fn activate(pin:& OutputFun<Pin=OutputPin<GpioRB=f3::memory_map::gpio::gpio_rb::Gpio>>){
+fn activate(pin:& OutputFun<Pin=OutputPin<GpioRB=f3::memory_map::gpioe::GpioRB>>){
     pin.turn(true);
 }
 */
@@ -52,45 +40,40 @@ fn activate(led:&Output<E8>){
 }
 */
 
+fn event(){
+    led1.turn(false);
+}
+
 #[inline(never)]
 #[no_mangle]
 pub fn main() -> ! {
+    unsafe{
+        f3::int_en();
+    }
+
     led1.enable();
-    led1.turn(true);
-    //led2.enable();
+    //led1.turn(true);
+
+    tim6.config();
 
     /*
-    {
-        let l2=led2.enable_temporary();
+    let state=true;
 
-        l2.turn(true);
-        delay::ms(500);
-        l2.turn(false);
-        delay::ms(500);
-    }
+    tim6.delay(500, Some(Box::new(move ||{ //TODO Hide Box::new if it is possible
+        led1.turn(state);
+    })), None);
+    */
 
-    //activate(&led1);
-    //activate(&led2);
-
-    //let arr[=[&led1,&led2];
-
-    //iprint!("Hello, world!");
 
     loop{
-        //led2.turn(false);
         led1.turn(true);
-        delay::ms(500);
+        tim6.wait(500);
         led1.turn(false);
-        //led2.turn(true);
-        delay::ms(500);
+        tim6.wait(500);
     }
 
-    //let e0=f3::gpioe::E0;
-    //panic!("Hello, world! {}",f3::stm32::pin::Pin::get_index(&e0));
-    //config(e0);
 
     //iprint!("Hello, world!");
-*/
     loop{}
 
 }
